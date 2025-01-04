b78d72dc54411d58a9419aaea43ed39c6ff99604
punnapavankumar9 <punnapavankumar9@gmail.com>
2025-01-04 15:03:19 +0530

    feat(event-catalog): disable ReactiveAuthenticationManager

--- event-catalog/src/main/java/com/punna/eventcatalog/config/SecurityConfig.java
+++ event-catalog/src/main/java/com/punna/eventcatalog/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import com.punna.eventcatalog.security.PopulateAuthentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableReactiveMethodSecurity
@@ -23,7 +25,10 @@ public class SecurityConfig {
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(p -> p
-                        .pathMatchers(HttpMethod.GET, "/api/v1/events/**", "/api/v1/venues/**", "/api/v1/seating-layout/**")
+                        .pathMatchers(HttpMethod.GET,
+                                "/api/v1/events/**",
+                                "/api/v1/venues/**",
+                                "/api/v1/seating-layout/**")
                         .permitAll()
                         .pathMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**")
                         .permitAll()
@@ -34,5 +39,11 @@ public class SecurityConfig {
                         .authenticated())
                 .addFilterAt(populateAuthentication, SecurityWebFiltersOrder.HTTP_BASIC)
                 .build();
+    }
+
+    @Bean
+    public ReactiveAuthenticationManager reactiveAuthenticationManager() {
+        return authentication -> Mono.error(new UnsupportedOperationException(
+                "Authentication is not supported here at downstream service."));
     }
 }
