5132440c8ee3f0cc3abcbd9da4be98f26fbcd4a7
punnapavankumar9 <punnapavankumar9@gmail.com>
2025-03-15 15:17:35 +0530

    feat(event-delay): add event-delay-service and configure versions in docker-compose.yml

---
+++ event-delay-engine/src/main/java/com/punna/eventbooking/event_delay_engine/EventDelayEngineApplication.java
@@ -0,0 +1,13 @@
+package com.punna.eventbooking.event_delay_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDelayEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDelayEngineApplication.class, args);
+	}
+
+}
---
+++ event-delay-engine/src/test/java/com/punna/eventbooking/event_delay_engine/EventDelayEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punna.eventbooking.event_delay_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDelayEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
