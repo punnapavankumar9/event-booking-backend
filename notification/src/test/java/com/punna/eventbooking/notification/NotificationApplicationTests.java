13d1bdc24eee2c6a48f5cf40605b37f0093526d8
punnapavankumar9 <punnapavankumar9@gmail.com>
2025-03-16 22:45:27 +0530

    feat(notification): add notification service

---
+++ notification/src/test/java/com/punna/eventbooking/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punna.eventbooking.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
